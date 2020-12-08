e5911b3b852d2df74aa064b6b8205bb066a81d1f
fpreciado3 <fpreciado3@ucol.mx>
2020-12-08 02:39:45 -0600

    implemented the for loop for the function

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ export default class App {
 
     sumatoriaSerieUno(numero) {
         let suma = 0;
+
+        for(let n = 1; n <= numero; n = n + 1){
+            suma = suma + 1/n;
+        }
     }
 }
 
