7d971ce7a5dc12c9a0b03ee52658b27e692323fb
fpreciado3 <fpreciado3@ucol.mx>
2020-12-08 02:40:52 -0600

    added the return of the variable & comment

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ export default class App {
         for(let n = 1; n <= numero; n = n + 1){
             suma = suma + 1/n;
         }
+
+        return suma;
     }
 }
 
 let app = new App();
+
+//Prueba de sumatoriaSerieUno()
+
